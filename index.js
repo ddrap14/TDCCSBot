e31dd0a4eff516157929ddf92e2b79eb4b4f3ef3
Derp <dylan.draper777@gmail.com>
2021-07-27 19:22:08 +1000

    this should work

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.BOT_TOKEN);
